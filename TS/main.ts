d01bdbe0094c2983fe621d0ffc116d8ab683cd16
Tyler McKane <polarbear36b@gmail.com>
2021-10-28 22:55:07 -0700

    Final Changes

--- TS/main.ts
+++ TS/main.ts
@@ -5,75 +5,152 @@ class VideoGame{
     isDigitalOnly:boolean;
 }
 
+// test code
 /*
-let myGame= new VideoGame
-myGame.title= "Mario";
+let myGame = new VideoGame();
+myGame.title = "Mario";
 myGame.rating = "E";
-myGame.isDigitalOnly = true
+myGame.isDigitalOnly = true;
 */
 
 window.onload = function(){
     let addBtn = 
-     <HTMLElement>document.querySelector ("input[type=button]");
+        <HTMLElement>document.querySelector("input[type=button]");
     addBtn.onclick = addVideoGame;
 }
 
+/**
+ * Clears all errors in the validation summary
+ */
+function clearAllErrors(){
+    let errSummary = getById("validation-summary");
+    errSummary.innerText = "";
+}
 
 function addVideoGame(){
-    if (isAllDataValid()){
-        let game = getVideoGame;
-        displayGame (game);
+    console.log("Add video game was called");
+    clearAllErrors();
+
+    if(isAllDataValid()){
+        let game:VideoGame = getVideoGame();
+        displayGame(game);
+    }
+    else{
+        displayRatingsLink();
     }
 }
-function getByID(id:string){
+
+function displayRatingsLink(){
+    let ratingsElements = document.querySelectorAll(".rating-error");
+    for(let i = 0; i < ratingsElements.length; i++){
+        let currElem = <HTMLElement>ratingsElements[i];
+        currElem.onclick = goToRatingsPage;
+        // currElem.innerHTML += "<a target='_blank' href='https://www.esrb.org/'>Click here for info</a>";
+    }
+}
+
+function goToRatingsPage(){
+    window.open("https://www.esrb.org/", "_blank");
+}
+
+function getById(id:string){
     return document.getElementById(id);
 }
+
+/**
+ * Gets all game data from the form
+ * and returns it in a VideoGame object
+ */
 function getVideoGame():VideoGame{
-        let game = new VideoGame();
+    let game = new VideoGame();
 
-        let titleInput = <HTMLInputElement>getByID("title");
-            game.title = titleInput.value;
+    // Populate with data from the form
+    let titleInput = <HTMLInputElement>getById("title");
+    game.title = titleInput.value;
 
-        let priceInput = <HTMLInputElement>getByID("price");
-            game.price = parseFloat(priceInput.value);
+    let priceInput = <HTMLInputElement>getById("price");
+    game.price = parseFloat(priceInput.value);
 
-        let ratingInput = <HTMLSelectElement>getByID("rating");
-            game.rating = ratingInput.value;
+    let ratingInput = <HTMLSelectElement>getById("rating");
+    game.rating = ratingInput.value;
 
-        let digitalOnly = <HTMLInputElement>getByID("online")
-        if(digitalOnly.checked){
-            game.isDigitalOnly = true;
-        }
-        else{
-            
-                game.isDigitalOnly = false;
-            }
-        console.log(game);
-        return game;
-
+    let digitalOnly = <HTMLInputElement>getById("online");
+    game.isDigitalOnly = digitalOnly.checked;
+    /*if(digitalOnly.checked){
+        game.isDigitalOnly = true;
+    }
+    else{
+        game.isDigitalOnly = false;
+    }*/
+    console.log(game);
+    return game;
 }
 
 function displayGame(myGame:VideoGame):void{
-        let displayDiv = getByID("display");
+    let displayDiv = getById("display");
 
-        let gameHeading = document.createElement("h2");
-        gameHeading.innerText = myGame.title;
+    // Create <h2> with game title
+    let gameHeading = document.createElement("h2");
+    gameHeading.innerText = myGame.title;
 
-        let gameInfo = document.createElement("p")
-        let notDigitalDisplay = "";
-        if (myGame.isDigitalOnly){
-            notDigitalDisplay = "not";
-        }
+    // Create paragraph with game details
+    let gameInfo = document.createElement("p");
+    let gameMediumDisplay = "";
+    if(myGame.isDigitalOnly){
+        gameMediumDisplay = "This is a digital only game.";
+    }
+    else{
+        gameMediumDisplay = "You can come buy a physical copy!";
+    }
+    gameInfo.innerText = `${myGame.title} has a rating of ${myGame.rating}. ` +
+        `It costs $${myGame.price.toFixed(2)}. ${gameMediumDisplay}`;
 
-        gameInfo.innerText = myGame.title +"has a rating of " +
-                        myGame.rating +". It cost " + myGame.price +
-                        ". It is " + notDigitalDisplay + " digital only";
-        gameInfo.innerText 
-        displayDiv.appendChild(gameHeading);
+    // Add <h2> in the <div id="display">
+    displayDiv.appendChild(gameHeading);
+    // Add <p> game info
+    displayDiv.appendChild(gameInfo);
+}
 
-        displayDiv.appendChild(gameInfo);
+function getInputById(id:string):HTMLInputElement{
+    return <HTMLInputElement>document.getElementById(id);
 }
 
 function isAllDataValid(){
-    return true;
+    let isValid = true;
+
+    let title = getInputById("title").value;
+    if(title == ""){
+        isValid = false;
+        addErrorMessage("Title is required");
+    }
+
+    let price = getInputById("price").value;
+    let priceValue = parseFloat(price);
+    if(price == "" || isNaN(priceValue)){
+        isValid = false;
+        addErrorMessage("Price is required and must be a number");
+    }
+
+    let rating = (<HTMLOptionElement>getById("rating")).value
+    if(rating == ""){
+        isValid = false;
+        addErrorMsgWithCustomClass("You must choose a rating!", "rating-error");
+    }
+
+    return isValid;
+}
+
+function addErrorMessage(errMsg:string) {
+    let errSummary = getById("validation-summary");
+    let errItem = document.createElement("li");
+    errItem.innerText = errMsg;
+    errSummary.appendChild(errItem);
+}
+
+function addErrorMsgWithCustomClass(errMsg:string, cssClass:string){
+    let errSummary = getById("validation-summary");
+    let errItem = document.createElement("li");
+    errItem.classList.add(cssClass);
+    errItem.innerText = errMsg;
+    errSummary.appendChild(errItem);
 }
--- js/main.js
+++ js/main.js
@@ -7,49 +7,97 @@ window.onload = function () {
     var addBtn = document.querySelector("input[type=button]");
     addBtn.onclick = addVideoGame;
 };
+function clearAllErrors() {
+    var errSummary = getById("validation-summary");
+    errSummary.innerText = "";
+}
 function addVideoGame() {
+    console.log("Add video game was called");
+    clearAllErrors();
     if (isAllDataValid()) {
-        var game = getVideoGame;
+        var game = getVideoGame();
         displayGame(game);
     }
+    else {
+        displayRatingsLink();
+    }
 }
-function getByID(id) {
+function displayRatingsLink() {
+    var ratingsElements = document.querySelectorAll(".rating-error");
+    for (var i = 0; i < ratingsElements.length; i++) {
+        var currElem = ratingsElements[i];
+        currElem.onclick = goToRatingsPage;
+    }
+}
+function goToRatingsPage() {
+    window.open("https://www.esrb.org/", "_blank");
+}
+function getById(id) {
     return document.getElementById(id);
 }
 function getVideoGame() {
     var game = new VideoGame();
-    var titleInput = getByID("title");
+    var titleInput = getById("title");
     game.title = titleInput.value;
-    var priceInput = getByID("price");
+    var priceInput = getById("price");
     game.price = parseFloat(priceInput.value);
-    var ratingInput = getByID("rating");
+    var ratingInput = getById("rating");
     game.rating = ratingInput.value;
-    var digitalOnly = getByID("online");
-    if (digitalOnly.checked) {
-        game.isDigitalOnly = true;
-    }
-    else {
-        game.isDigitalOnly = false;
-    }
+    var digitalOnly = getById("online");
+    game.isDigitalOnly = digitalOnly.checked;
     console.log(game);
     return game;
 }
 function displayGame(myGame) {
-    var displayDiv = getByID("display");
+    var displayDiv = getById("display");
     var gameHeading = document.createElement("h2");
     gameHeading.innerText = myGame.title;
     var gameInfo = document.createElement("p");
-    var notDigitalDisplay = "";
+    var gameMediumDisplay = "";
     if (myGame.isDigitalOnly) {
-        notDigitalDisplay = "not";
+        gameMediumDisplay = "This is a digital only game.";
     }
-    gameInfo.innerText = myGame.title + "has a rating of " +
-        myGame.rating + ". It cost " + myGame.price +
-        ". It is " + notDigitalDisplay + " digital only";
-    gameInfo.innerText;
+    else {
+        gameMediumDisplay = "You can come buy a physical copy!";
+    }
+    gameInfo.innerText = myGame.title + " has a rating of " + myGame.rating + ". " +
+        ("It costs $" + myGame.price.toFixed(2) + ". " + gameMediumDisplay);
     displayDiv.appendChild(gameHeading);
     displayDiv.appendChild(gameInfo);
 }
+function getInputById(id) {
+    return document.getElementById(id);
+}
 function isAllDataValid() {
-    return true;
+    var isValid = true;
+    var title = getInputById("title").value;
+    if (title == "") {
+        isValid = false;
+        addErrorMessage("Title is required");
+    }
+    var price = getInputById("price").value;
+    var priceValue = parseFloat(price);
+    if (price == "" || isNaN(priceValue)) {
+        isValid = false;
+        addErrorMessage("Price is required and must be a number");
+    }
+    var rating = getById("rating").value;
+    if (rating == "") {
+        isValid = false;
+        addErrorMsgWithCustomClass("You must choose a rating!", "rating-error");
+    }
+    return isValid;
+}
+function addErrorMessage(errMsg) {
+    var errSummary = getById("validation-summary");
+    var errItem = document.createElement("li");
+    errItem.innerText = errMsg;
+    errSummary.appendChild(errItem);
+}
+function addErrorMsgWithCustomClass(errMsg, cssClass) {
+    var errSummary = getById("validation-summary");
+    var errItem = document.createElement("li");
+    errItem.classList.add(cssClass);
+    errItem.innerText = errMsg;
+    errSummary.appendChild(errItem);
 }
