26e222a5689f34ff1afc473d6127b175463704af
Tyler McKane <polarbear36b@gmail.com>
2021-10-28 22:36:37 -0700

    just need validation left one error

--- TS/main.ts
+++ TS/main.ts
@@ -1,8 +1,8 @@
 class VideoGame{
-    title: string;
-    price: number;
-    rating: string;
-    isDigitalOnly: boolean;
+    title:string;
+    price:number;
+    rating:string;
+    isDigitalOnly:boolean;
 }
 
 /*
@@ -18,21 +18,60 @@ window.onload = function(){
     addBtn.onclick = addVideoGame;
 }
 
-function addVideoGame(){
-    console.log("Add video game was called")
 
-    if(isAllDataValid()){
+function addVideoGame(){
+    if (isAllDataValid()){
         let game = getVideoGame;
-        displayGame(game);
+        displayGame (game);
     }
 }
-
+function getByID(id:string){
+    return document.getElementById(id);
+}
 function getVideoGame():VideoGame{
+        let game = new VideoGame();
+
+        let titleInput = <HTMLInputElement>getByID("title");
+            game.title = titleInput.value;
+
+        let priceInput = <HTMLInputElement>getByID("price");
+            game.price = parseFloat(priceInput.value);
+
+        let ratingInput = <HTMLSelectElement>getByID("rating");
+            game.rating = ratingInput.value;
+
+        let digitalOnly = <HTMLInputElement>getByID("online")
+        if(digitalOnly.checked){
+            game.isDigitalOnly = true;
+        }
+        else{
+            
+                game.isDigitalOnly = false;
+            }
+        console.log(game);
+        return game;
 
 }
 
-function displayGame(myGame:videoGame):void{
+function displayGame(myGame:VideoGame):void{
+        let displayDiv = getByID("display");
 
+        let gameHeading = document.createElement("h2");
+        gameHeading.innerText = myGame.title;
+
+        let gameInfo = document.createElement("p")
+        let notDigitalDisplay = "";
+        if (myGame.isDigitalOnly){
+            notDigitalDisplay = "not";
+        }
+
+        gameInfo.innerText = myGame.title +"has a rating of " +
+                        myGame.rating +". It cost " + myGame.price +
+                        ". It is " + notDigitalDisplay + " digital only";
+        gameInfo.innerText 
+        displayDiv.appendChild(gameHeading);
+
+        displayDiv.appendChild(gameInfo);
 }
 
 function isAllDataValid(){
--- js/main.js
+++ js/main.js
@@ -8,15 +8,47 @@ window.onload = function () {
     addBtn.onclick = addVideoGame;
 };
 function addVideoGame() {
-    console.log("Add video game was called");
     if (isAllDataValid()) {
         var game = getVideoGame;
         displayGame(game);
     }
 }
+function getByID(id) {
+    return document.getElementById(id);
+}
 function getVideoGame() {
+    var game = new VideoGame();
+    var titleInput = getByID("title");
+    game.title = titleInput.value;
+    var priceInput = getByID("price");
+    game.price = parseFloat(priceInput.value);
+    var ratingInput = getByID("rating");
+    game.rating = ratingInput.value;
+    var digitalOnly = getByID("online");
+    if (digitalOnly.checked) {
+        game.isDigitalOnly = true;
+    }
+    else {
+        game.isDigitalOnly = false;
+    }
+    console.log(game);
+    return game;
 }
 function displayGame(myGame) {
+    var displayDiv = getByID("display");
+    var gameHeading = document.createElement("h2");
+    gameHeading.innerText = myGame.title;
+    var gameInfo = document.createElement("p");
+    var notDigitalDisplay = "";
+    if (myGame.isDigitalOnly) {
+        notDigitalDisplay = "not";
+    }
+    gameInfo.innerText = myGame.title + "has a rating of " +
+        myGame.rating + ". It cost " + myGame.price +
+        ". It is " + notDigitalDisplay + " digital only";
+    gameInfo.innerText;
+    displayDiv.appendChild(gameHeading);
+    displayDiv.appendChild(gameInfo);
 }
 function isAllDataValid() {
     return true;
